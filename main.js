5477ebdc4719517bee0bff510e79cad26e152331
MioRain <ericzero1593@hotmail.com>
2023-05-15 04:10:12 +0800

    feat: add vote counting

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
 const gojo = document.querySelector('.gojo')
+const gojoBar = document.querySelector('.gojo-bar')
 const sukuna = document.querySelector('.sukuna')
+const sukunaBar = document.querySelector('.sukuna-bar')
 const vote = document.querySelector('.vote')
 const close = document.querySelector('.close')
 const submit = document.querySelector('.submit')
 
-const postUrl = 'https://script.google.com/macros/s/AKfycbwFLz67VS_dxKgAnbFguTcWMD6YYS8bGm3wPaIM5a_p6yrxXRHNzgJPVyWUbHjGQd1w/exec'
+const url = 'https://script.google.com/macros/s/AKfycbyT8jwZ-XwiyCngerESkJgOpwv0unh1KYI5ZMaEUh_RrKEpDbnmmMxg-_I693SBr4SC/exec'
 let time = ''
 let voter = ''
 let supporter = ''
 let content = ''
+
+voteCounting()
+
+async function voteCounting() {
+  let gojoCount = 0
+  let sukunaCount = 0
+  await $.get(url, function (e) {
+    e.forEach((name) => {
+      if (name[0] === '五條') {
+        gojoCount++
+      } else if (name[0] === '宿儺') {
+        sukunaCount++
+      }
+    })
+  })
+  let deno = gojoCount + sukunaCount
+  gojoBar.innerText = String(gojoCount / deno).slice(2, 4) + '%'
+  gojoBar.style.width = `${String(gojoCount / deno).slice(2, 4)}%`
+  sukunaBar.innerText = String(sukunaCount / deno).slice(2, 4) + '%'
+  sukunaBar.style.width = `${String(sukunaCount / deno).slice(2, 4)}%`
+}
+
 
 gojo.addEventListener('click', () => {
   if (JSON.parse(localStorage.getItem('voted'))) {
@@ -49,14 +73,14 @@ submit.addEventListener('click', () => {
   content = document.querySelector('.content').value
 
   if (voter && content) {
-    $.post(postUrl, {
+    $.post(url, {
       time: time,
       voter: voter,
       supporter: supporter,
       content: content
     }, function (e) {
-      console.log(e);
-    });
+      console.log(e)
+    })
     localStorage.setItem('voted', true)
     alert('投票完成')
     close.click()
